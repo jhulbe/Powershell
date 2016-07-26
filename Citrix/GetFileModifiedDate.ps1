1f3830b531cab765799e7d52f76f6a92c8415ba2
jhulbe <jhulbe@jhulbe.com>
2016-07-26 10:32:29 -0400

    aster

---
+++ Citrix/GetFileModifiedDate.ps1
@@ -0,0 +1,2 @@
+$profilenames = get-childitem \\server\user_profile$ | select name -ExpandProperty name | Sort-Object name
+foreach ($profile in $profilenames){Write-host $profile @(get-itemproperty \\server\user_profile$\$profile\UPM_Profile\NTUSER.dat -ErrorAction SilentlyContinue).lastwritetime;}
