007b84677dc5756d2cf1ffa01def01e0c873d978
jhulbe <jhulbe@jhulbe.com>
2016-11-16 10:14:13 -0500

    Update GetFileCountForCookiesDirectory.ps1

--- Citrix/GetFileCountForCookiesDirectory.ps1
+++ Citrix/GetFileCountForCookiesDirectory.ps1
@@ -1,4 +1,4 @@
 ##Example Grabs the subdirectores in the user_ctx_profile$ path and then looks in each profile for the cookies and then list the number of files in each profile. 
 
 $profilenames = @(get-childitem \\server\user_ctx_profile$ | sort-object name | select -ExpandProperty name );
-foreach ($profile in $profilenames){Write-host $profile (get-childitem \\server.cfins.com\user_ctx_profile$\$profile\UPM_Profile\AppData\Roaming\Microsoft\Windows\Cookies -ErrorAction SilentlyContinue).count}
+foreach ($profile in $profilenames){Write-host $profile (get-childitem \\server\user_ctx_profile$\$profile\UPM_Profile\AppData\Roaming\Microsoft\Windows\Cookies -ErrorAction SilentlyContinue).count}
