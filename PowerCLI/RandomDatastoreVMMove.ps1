67960f86491467b9759d854ed46f0f285742e99e
jhulbe <jhulbe@jhulbe.com>
2016-11-29 19:30:07 -0500

    Datastore VM Move

---
+++ PowerCLI/RandomDatastoreVMMove.ps1
@@ -0,0 +1,13 @@
+﻿<#
+Command moves everything in old datastores to new datastores by randomly choosing the new data store name from the array. 
+#>
+
+
+$SERVERS = Get-Datastore | Where {$_.Name -like "nim-citrix*"} | Get-VM | Select Name
+$DATASTOREARRAY = 'FC_SSD_Citrix03','FC_SSD_Citrix04','FC_SSD_Citrix05','_FC_SSD_Citrix06'
+$DATASTORE = $DATASTOREARRAY[(Get-Random -Maximum ([array]$DATASTOREARRAY).count)]
+
+Foreach($Server in $Servers){
+Write-host "Moving $SERVER to $Datastore"
+Move-VM $Server -Datastore $DATASTORE
+}
