07a24e244c7e85e4d5a3f573fbec1142f4c1dd32
jhulbe <jhulbe@jhulbe.com>
2016-12-01 08:07:01 -0500

    Update errors found in vmmove

--- PowerCLI/RandomDatastoreVMMove.ps1
+++ PowerCLI/RandomDatastoreVMMove.ps1
@@ -3,7 +3,7 @@ Command moves everything in old datastores to new datastores by randomly choosin
 #>
 
 
-$SERVERS = Get-Datastore | Where {$_.Name -like "nim-citrix*"} | Get-VM | Select Name
+$SERVERS = Get-Datastore | Where {$_.Name -like "nim-citrix*"} | Get-VM | Select Name -ExpandProperty name
 $DATASTOREARRAY = 'FC_SSD_Citrix03','FC_SSD_Citrix04','FC_SSD_Citrix05','_FC_SSD_Citrix06'
 $DATASTORE = $DATASTOREARRAY[(Get-Random -Maximum ([array]$DATASTOREARRAY).count)]
 
