3f7a690bd2f71a601356638164cc614754c75b96
jhulbe <jhulbe@jhulbe.com>
2016-12-16 15:26:58 -0500

    GrouplikeD
    added comments to groupliked

--- Functions/GroupLikeD.ps1
+++ Functions/GroupLikeD.ps1
@@ -1,3 +1,12 @@
+<#
+ function searches for a group and then list all matching groups with the Description included.
+ similar to grouplike function, but includes the description. 
+
+ Ex. GrouplikeD citrix 
+
+ gives all the AD groups with Citrix in the name.
+ #>
+
 function GroupLikeD([string]$name) {
    Get-ADGroup -Filter "name -like '*$name*'" -Properties * | select Name,Description | Sort-object Description
 }
