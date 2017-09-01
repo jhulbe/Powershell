141b48c4c9c154c807932a7baa971d304947783f
jhulbe <jhulbe@jhulbe.com>
2017-09-01 10:42:05 -0400

    Update XA-CreateExcelAppsFromDir.ps1
    add permission stuff

--- Citrix/XA-CreateExcelAppsFromDir.ps1
+++ Citrix/XA-CreateExcelAppsFromDir.ps1
@@ -1,24 +1,29 @@
-$AppName = (gci C:\ExcelApps).fullname
+$AppName = (gci C:\EXCELAPPS).fullname
+
+        #BUILD ALL THE PROPERTIES FROM THE NEW-BROKERAPPLICATION COMMAND ONE APP AT A TIME
 
 Foreach ($App in $Appname){
 
-  $ShortName = ($App).replace("C:\ExcelApps\","")
-  $PublishedName = "Excel-$ShortName"
-  $WorkingDirectory = ($App).replace("C:","%SystemDrive%")
-  $CLE = (gci $App -recurse | Where{($_.fullname -like "*.cmd") -or ($_.fullname -like "*.bat")}).fullname.replace("C:","%SystemDrive%")
-  $AdminFolder = "Excel Apps\"
-  $CF = "Excel Apps"
+$ShortName = ($App).replace("C:\EXCELAPPS\","")
+$PublishedName = "Excel-$ShortName"
+$Name = ($App).replace("C:\EXCELAPPS\","Excel Apps\")
+$WorkingDirectory = ($App).replace("C:","%SystemDrive%")
+$CLE = (gci $App -recurse | Where{($_.fullname -like "*.cmd") -or ($_.fullname -like "*.bat")}).fullname.replace("C:","%SystemDrive%")
+$AdminFolder = "Excel Apps\"
+$CF = "Excel Apps"
 
-          Write-host $PublishedName -ForegroundColor Yellow
+Write-host Creating $ShortName -ForegroundColor Cyan
+
+        #CREATE THE APP
 
 New-BrokerApplication `
 -Name $PublishedName `
 -CommandLineExecutable $CLE `
--DesktopGroup "US-Desktops" `
+-DesktopGroup "US-DESKTOPS=" `
 -AdminFolder $AdminFolder `
 -ApplicationType "HostedOnDesktop" `
 -BrowserName $PublishedName `
--ClientFolder "$CF" `
+-ClientFolder "Excel Apps" `
 -Enabled $True `
 -IconUid 10 `
 -PublishedName $PublishedName `
@@ -26,7 +31,33 @@ New-BrokerApplication `
 -ShortcutAddedToStartMenu $False `
 -UserFilterEnabled $true `
 -Visible $True `
--WorkingDirectory $WorkingDirectory 
+-WorkingDirectory $WorkingDirectory | out-null
 
-           sleep 10
+Write-host "Adding Permissions"
+        #MATCH THE PERMISSIONS ON THE START MENU TO THE APP, GATHER LIST OF USERS AND GROUPS
+
+$Groups = gci \\netapp\ctxusershare\Start\ExcelApps | ?{$_.name -like "*$shortname*"}| Get-ntfsaccess | ?{($_.account -like "DOMAIN\*") -and ($_.account -notlike "*CITRIX_ADMINS*")} 
+Foreach($G in $Groups){
+$Shortgroup = $G.account
+$PERMGROUP = $shortgroup -replace [Regex]::Escape("DOMAIN\"),""
+
+
+          #CHECK IF IT’S A USER OR A GROUP, ADD ALL USERS OF GROUPS OR JUST THE USER
+
+foreach ($User in $PERMGROUP) { 
+try { 
+Get-ADUser -Identity $User | Out-Null 
+Add-BrokerUser -Name $User -Application "Excel Apps\$PublishedName"
+} 
+catch { 
+Get-adgroupMember $PERMGROUP | %{Add-BrokerUser -Name $_.samaccountname -Application "Excel Apps\$PublishedName"} 
+} 
+} 
+          #ADD CITRIX_ADMINS
+
+add-brokeruser -Name DOMAIN\CITRIX_ADMINS -Application "Excel Apps\$PublishedName"
+}
+          #HAD TO ADD A SMALL SLEEP BETWEEN EACH APP OR IT IMPLODES LIKE THE DEATHSTAR, IT’S JUST TOO FAST
+
+sleep 2
 }
