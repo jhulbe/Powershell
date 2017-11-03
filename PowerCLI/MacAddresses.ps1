8e710bcba7f256a5ccae5e8b25dbc3412973457b
jhulbe <jhulbe@jhulbe.com>
2017-11-03 11:43:25 -0400

    Update MacAddresses.ps1

--- PowerCLI/MacAddresses.ps1
+++ PowerCLI/MacAddresses.ps1
@@ -1,3 +1,5 @@
+#Snippet of code from a deployment script that grabs the mac addresses from newly created VMs to import into a DHCP Reservation code. 
+
 $VM = Get-content .\systems.txt
 $mac = (Get-NetworkAdapter -vm $vm).MacAddress 
 format-list
