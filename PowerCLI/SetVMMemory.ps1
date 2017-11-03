fbee227142ee74dc31d3851d33185498752015cb
jhulbe <jhulbe@jhulbe.com>
2017-11-03 11:44:42 -0400

    Update SetVMMemory.ps1

--- PowerCLI/SetVMMemory.ps1
+++ PowerCLI/SetVMMemory.ps1
@@ -1,4 +1,8 @@
+#Upgrading VMs from 16 to 24GB, used this 
+
 $VMS = Get-Content .\systems.txt
-foreach ($VM in $VMS){Get-VM $VM | Shutdown-VMGuest -Confirm:$False}
-foreach ($VM in $VMS){Get-VM $VM | Set-VM -memorygb 24 -Confirm:$False}
-foreach ($VM in $VMS){Start-VM $VM}
+foreach ($VM in $VMS){
+Get-VM $VM | Shutdown-VMGuest -Confirm:$False
+Get-VM $VM | Set-VM -memorygb 24 -Confirm:$False
+Start-VM $VM
+}
